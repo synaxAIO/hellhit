5c56f5d58a6a6c4206d76f32702a44653e441e80
synaxAIO <129850161+synaxAIO@users.noreply.github.com>
2023-04-14 09:47:40 +0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,9 @@
+import os
+
+os.system('pip install colorama')
+os.system('pip install pystyle')
+os.system('pip install secrets')
+os.system('pip install browser_cookie3')
+os.system('pip install cv2')
+os.system('pip install ImageGrab')
+os.system('pip install browser_history.browsers')
